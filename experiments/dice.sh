2890a6324cede71eb79372560dbc6beebf856501
Cristian Garcia <cgarcia.e88@gmail.com>
2019-01-19 22:35:48 -0500

    save

--- experiments/dice.sh
+++ experiments/dice.sh
@@ -1,13 +1,13 @@
 
 # data
-OUTPUT_DIR='/data/ai_fund/data/deep-casino/dr-1'
-N_SAMPLES=20000
+OUTPUT_DIR='experiments/data/dice/datasets/v1'
+N_SAMPLES=100000
 
 N_OBJECTS='(3,6)' 
-OBJECTS_PATTERN='experiments/images/dice/**/*'  
-BACKGROUNDS_PATTERN='experiments/images/backgrounds/*' 
+OBJECTS_PATTERN='experiments/data/dice/images/dice/**/*' 
+BACKGROUNDS_PATTERN='experiments/data/dice/images/backgrounds/*' 
 ROTATION_ANGLES=360  
-OBJECT_RESIZE=40
+OBJECT_RESIZE=37
 BACKGROUND_RESIZE='(512,512)' 
 IOU_THRESHOLD=0.0
 OBJECT_CHANNEL_MULTIPLY=True
@@ -15,24 +15,29 @@ BACKGROUND_CHANNEL_MULTIPLY=True
 OBJECT_CHANNEL_INVERT=True
 BACKGROUND_CHANNEL_INVERT=False
 BACKGROUND_ROTATE=True
-OBJECT_SCALE=1.2
+OBJECT_SCALE=1.3
 OUTPUT_EXTENSION="jpg"
 
 # job
-WORKERS=8
+WORKERS=9
 
 # args
 for var in "$@"; do
 
     if [[ "$var" == "--toy" ]]; then
-        OUTPUT_DIR='experiments/images/results'
+        OUTPUT_DIR='experiments/data/dice/datasets/tmp'
         N_SAMPLES=100
+    fi
+    if [[ "$var" == "--rm" ]]; then
+        REMOVE=true
     fi
     
 done
 
-# clear dir
-rm -fr $OUTPUT_DIR
+if ${REMOVE:-false} ;then
+    rm -fr $OUTPUT_DIR
+fi
+
 
 # run
 domain-randomization object-detection \
