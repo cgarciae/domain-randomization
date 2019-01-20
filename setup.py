82a778411f66febd1872cce456991b9d6b380b0f
Cristian Garcia <cgarcia.e88@gmail.com>
2019-01-20 18:51:43 -0500

    install pascal voq writter

--- setup.py
+++ setup.py
@@ -1,25 +1,30 @@
 import os
 from setuptools import setup, find_packages
+import subprocess
 
 
-setup(
-    name = "domain_randomization",
-    version = "0.0.2",
-    author = "Cristian Garcia",
-    author_email = "cgarcia.e88@gmail.com",
-    description = ("An object-like dictionary"),
-    license = "MIT",
-    keywords = [],
-    url = "https://github.com/cgarciae/domain_randomization",
-   	packages = find_packages(),
-    package_data={
-        '': ['LICENCE', 'requirements.txt', 'README.md', 'CHANGELOG.md'],
-    },
-    include_package_data = True,
-    install_requires = open("requirements.txt").read().strip().split("\n"),
-    entry_points = {
-        'console_scripts': [
-            'domain-randomization = domain_randomization.cli:main'
-        ],
-    }
-)
+if __name__ == "__main__":
+    
+    subprocess.call("pip install git+https://github.com/cgarciae/pascal_voc_writer")
+
+    setup(
+        name = "domain_randomization",
+        version = "0.0.2",
+        author = "Cristian Garcia",
+        author_email = "cgarcia.e88@gmail.com",
+        description = ("An object-like dictionary"),
+        license = "MIT",
+        keywords = [],
+        url = "https://github.com/cgarciae/domain_randomization",
+        packages = find_packages(),
+        package_data={
+            '': ['LICENCE', 'requirements.txt', 'README.md', 'CHANGELOG.md'],
+        },
+        include_package_data = True,
+        install_requires = open("requirements.txt").read().strip().split("\n"),
+        entry_points = {
+            'console_scripts': [
+                'domain-randomization = domain_randomization.cli:main'
+            ],
+        }
+    )
